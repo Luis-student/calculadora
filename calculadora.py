1afa1a0fadb670122f3bcf164a8a82bbf0ebc2dc
jaymeluis23@gmail.com <jaymeluis23@gmail.com>
2026-03-10 08:05:34 -0300

    add soma

---
+++ calculadora.py
@@ -0,0 +1,4 @@
+print('======CALCULADAORA======')
+num1 = int(input("ensira o primerio numero: "))
+operação = input("encolha a operação (+, -, *, /): ")
+num2 = int(input('ensira o segundo numero: '))

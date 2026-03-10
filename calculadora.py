0169a6ad325ba3a47f5e7be94e6558863cc1e665
jaymeluis23@gmail.com <jaymeluis23@gmail.com>
2026-03-10 08:07:59 -0300

    add subtração

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,11 @@
+def soma(a, b):
+    som = a + b 
+    return som
+
+def subtracao(a, b):
+    sub = a - b 
+    return sub
+
 print('======CALCULADAORA======')
 num1 = int(input("ensira o primerio numero: "))
 operação = input("encolha a operação (+, -, *, /): ")

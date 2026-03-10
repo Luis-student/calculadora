4dde5162669eac96a347a4e3ab430e0fb35952a1
jaymeluis23@gmail.com <jaymeluis23@gmail.com>
2026-03-10 08:26:46 -0300

    função para as operações

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,4 @@
+#funções para as operações basicas 
 def soma(a, b):
     som = a + b 
     return som
@@ -10,11 +11,33 @@ def multiplicacao(a, b):
     mult = a * b 
     return mult 
 
-def divisão(a, b):
+def divisao(a, b):
     div = a / b 
     return div 
 
-print('======CALCULADAORA======')
-num1 = int(input("ensira o primerio numero: "))
-operação = input("encolha a operação (+, -, *, /): ")
-num2 = int(input('ensira o segundo numero: '))
+
+#funcao da calculadora 
+def calculadora(a, b, c):
+    if c == "+":
+        return soma(a, b)
+    
+    elif c == "-" : 
+        return subtracao(a, b) 
+
+    elif c == "*":
+        return multiplicacao(a, b)
+
+    elif c == "/":
+        return divisao(a, b) 
+    
+
+
+
+   
+
+
+
+
+
+    
+
